e7fb57a0a2c7bcd3459ebdc794ebbca1adb9e6ed
Bogatashki <41234043+Bogatashki@users.noreply.github.com>
2018-08-01 16:04:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumner = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int forthNumber = int.Parse(Console.ReadLine());
+
+            string debitCardNumber = $"{firstNumber:D4} {secondNumner:D4} {thirdNumber:D4} {forthNumber:D4}";
+            Console.WriteLine(debitCardNumber);
+        }
+    }
+}
