00335c86bb8bba46007657f1b0aa60b46e5157ac
Bogatashki <41234043+Bogatashki@users.noreply.github.com>
2018-08-01 17:04:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04._Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            double sugarContent = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{volume}ml {name}:");
+            double totalCalories = volume * energyContent / 100.0;
+            double totalSugars = volume * sugarContent / 100;
+            Console.WriteLine($"{totalCalories}kcal, {totalSugars}g sugars");
+        }
+    }
+}
