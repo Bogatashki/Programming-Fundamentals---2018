9fc87d3d1b6ebd8a5b51cda508ecaf1f326e08c8
Bogatashki <41234043+Bogatashki@users.noreply.github.com>
2018-08-01 16:57:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _02._Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side1 = double.Parse(Console.ReadLine());
+            double side2 = double.Parse(Console.ReadLine());
+
+            double area = side1 * side2;
+            Console.WriteLine($"{area:f2}");
+        }
+    }
+}
